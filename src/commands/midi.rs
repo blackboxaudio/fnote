4cc5ee20abe03f525c7543488806eb95b8b1de59
Matthew Maxwell <maxwellmattryan@gmail.com>
2024-10-30 19:36:38 -0500

    Add conversion function

--- src/commands/midi.rs
+++ src/commands/midi.rs
@@ -11,23 +11,37 @@ use crate::{
 pub struct MidiCommand {
     /// The MIDI note number to use.
     #[structopt(parse(try_from_str = try_midi_note_number_from_str))]
-    pub midi_note_number: usize,
+    pub midi_note_number: u8,
 }
 
 #[async_trait]
 impl Command for MidiCommand {
     async fn run(&self) -> FNoteResult<()> {
-        println!("Converting MIDI note number: {:?}", self.midi_note_number);
+        println!("MIDI: {}", self.midi_note_number);
+        println!(
+            "Note: {}",
+            midi_note_number_to_music_note(self.midi_note_number).unwrap()
+        );
 
         Ok(())
     }
 }
 
+fn midi_note_number_to_music_note(midi_note_number: u8) -> Option<String> {
+    let semitone_to_note = ["C", "C#", "D", "Eb", "E", "F", "F#", "G", "Ab", "A", "Bb", "B"];
+
+    let octave = (midi_note_number / 12) as i8 - 1;
+    let semitone_offset = (midi_note_number % 12) as usize;
+    let note = semitone_to_note.get(semitone_offset).unwrap();
+
+    Some(format!("{}{}", note, octave))
+}
+
 /// Parses a MIDI note number from a string.
-fn try_midi_note_number_from_str(arg: &str) -> FNoteResult<usize> {
+fn try_midi_note_number_from_str(arg: &str) -> FNoteResult<u8> {
     let regex = Regex::new(r"^(?:\d|[1-9]\d|1[01]\d|12[0-7])$").unwrap();
     if regex.is_match(arg) {
-        Ok(arg.parse::<usize>().unwrap())
+        Ok(arg.parse::<u8>().unwrap())
     } else {
         Err(FNoteError::InvalidMidiNoteNumber(arg.to_string()))
     }
